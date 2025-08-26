b9a47e3f1798c6e6e442547aa27ba99c12eacb52
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-08-26 10:04:53 +0300

    fix: moved featured events into home page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,8 @@
-import { getFeaturedEvents } from '../../dummy-data';
-import EventsList from '../../components/events/List';
-
 const EventsPage = () => {
-	const events = getFeaturedEvents();
 
 	return (
 		<div>
 			<h1>EventsPage</h1>
-			<EventsList events={events} />
 		</div>
 	);
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,13 @@
+import EventsList from '../components/events/List';
+import { getFeaturedEvents } from '../dummy-data';
+
 const HomePage = () => {
+	const events = getFeaturedEvents();
+
 	return (
 		<div>
 			<h1>HomePage</h1>
+			<EventsList events={events} />
 		</div>
 	);
 };
